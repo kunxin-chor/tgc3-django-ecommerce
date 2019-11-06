e8202369ee14714f204513a71058539b28e6bd18
Ubuntu <ubuntu@ip-172-31-20-241.ap-southeast-1.compute.internal>
2019-11-06 07:11:37 +0000

    Added a product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    sku = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=False)
+    cost = models.IntegerField(blank=False)
+    quantity = models.IntegerField(blank=False, default=0)
